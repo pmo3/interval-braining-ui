c62ce0c4153d43a13b9d7766c5068e369093c094
Danny Guinther <dannyguinther@gmail.com>
2014-03-10 08:52:47 -0400

    Fix test_hepler load order

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,19 @@
 ENV['RAILS_ENV'] ||= 'test'
 
+require File.expand_path('../dummy/config/environment.rb',  __FILE__)
+require 'minitest/autorun'
+require 'mocha/setup'
+require 'shoulda/context'
+require 'factory_girl_rails'
+require 'faker'
+require 'rails/test_help'
+
+Rails.application.eager_load!
+Rails.backtrace_cleaner.remove_silencers!
+
+# Load support files
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
 if ENV['CI']
   require 'simplecov'
   require 'coveralls'
@@ -14,18 +28,3 @@ if ENV['CI']
     add_filter 'test/'
   end
 end
-
-require File.expand_path('../dummy/config/environment.rb',  __FILE__)
-require 'minitest/autorun'
-require 'mocha/setup'
-require 'shoulda/context'
-require 'factory_girl_rails'
-require 'faker'
-require 'rails/test_help'
-
-
-Rails.application.eager_load!
-Rails.backtrace_cleaner.remove_silencers!
-
-# Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
