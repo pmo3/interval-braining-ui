ca8f0208ce0e759b04c0ba7f4107dd8abee14068
Danny Guinther <dannyguinther@gmail.com>
2014-03-14 07:25:43 -0400

    Add simple root action

--- app/controllers/interval_braining_ui/application_controller.rb
+++ app/controllers/interval_braining_ui/application_controller.rb
@@ -1,8 +1,14 @@
 module IntervalBrainingUI
   class ApplicationController < ActionController::Base
 
+    def index
+      render :nothing => true, :layout => 'interval_braining_ui/application'
+    end
+
+
     def templates
       render 'templates', :layout => false
     end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ require 'application_manifest'
 
 IntervalBrainingUI::Engine.routes.draw do
 
+  root :to => 'application#index'
+
   get 'application.manifest' => ApplicationManifest
   get 'templates' => 'application#templates'
 
