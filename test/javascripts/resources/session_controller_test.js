9ff8fa92b46c575485985d095e04932d03699d5f
Patrick O'Brien <obiedman@gmail.com>
2014-06-13 20:12:41 -0400

    Test for successful sign in

--- test/javascripts/resources/session_controller_test.js
+++ test/javascripts/resources/session_controller_test.js
@@ -9,9 +9,10 @@ describe('SessionsController', function(){
 
     sessionResource = $injector.get('sessionResource');
     growlNotifications = $injector.get('growlNotifications');
+    currentUser = $injector.get('currentUser');
 
     spyOn(growlNotifications, 'add');
-    sessionsController = $controller('SessionsController', {sessionResource : sessionResource, growlNotifications : growlNotifications});
+    sessionsController = $controller('SessionsController', {sessionResource : sessionResource, growlNotifications : growlNotifications, currentUser : currentUser});
 
     });
       });
@@ -22,9 +23,9 @@ describe('SessionsController', function(){
     }));
 
 
-  it('should add a growl notification when sign in fails', inject(function($httpBackend){
-    $httpBackend.expect('POST', '/api/v1/users/sign_in.json').respond(401, '');
-    $httpBackend.expect('GET', '/api/v1/users/me.json', null).respond(401, ''); 
+  it('adds a growl notification when sign in fails', inject(function($httpBackend){
+    $httpBackend.expect('POST', '/api/v1/users/sign_in.json', sessionData).respond(401, '');
+    $httpBackend.expect('GET', '/api/v1/users/me.json').respond(401, ''); 
     $httpBackend.expect('GET', '/assets/interval_braining_ui/layouts/navbar_page_header_layout.html').respond(null);
     $httpBackend.expect('GET', '/assets/interval_braining_ui/sessions/new.html').respond(null);
     sessionsController.email = sessionData.email;
@@ -35,4 +36,19 @@ describe('SessionsController', function(){
 
     expect(growlNotifications.add).toHaveBeenCalled();
     }));
+
+  it('sets currentUser on successful POST', inject(function($httpBackend){
+    spyOn(currentUser, 'set');
+    $httpBackend.when('POST', '/api/v1/users/sign_in.json', sessionData).respond({id: 1, email: sessionData.email});
+    $httpBackend.expect('GET', '/api/v1/users/me.json').respond({id: 1, email: sessionData.email}); 
+    $httpBackend.expect('GET', '/assets/interval_braining_ui/layouts/navbar_page_header_layout.html').respond(null);
+
+    sessionsController.email = sessionData.email;
+    sessionsController.password = sessionData.password;
+    sessionsController.remember_me = sessionData.remember_me;
+    sessionsController.submit();
+    $httpBackend.flush();
+    expect(currentUser.set).toHaveBeenCalled();
+    expect(currentUser.set).toHaveBeenCalledWith({id: 1, email: sessionData.email});
+  }));
 });
