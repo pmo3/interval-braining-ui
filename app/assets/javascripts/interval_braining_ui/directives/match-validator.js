ec04c435166a02ea955d0adbdb3ab97dbed4c74d
Patrick O'Brien <obiedman@gmail.com>
2014-06-30 17:08:29 -0400

    Minor formatting changes
    Fixed indentation

--- app/assets/javascripts/interval_braining_ui/directives/match-validator.js
+++ app/assets/javascripts/interval_braining_ui/directives/match-validator.js
@@ -2,17 +2,17 @@
   var app = angular.module('IntervalBraining');
 
   app.directive('matchValidator', function() {
-      return {
-        require: 'ngModel',
-        link: function(scope, element, attrs, ngModel) {
-          ngModel.$parsers.push(function(value) { 
-            ngModel.$setValidity('match', value == scope.$eval(attrs.matchValidator));
-            return value;
-          });
-        }
+    return {
+      require: 'ngModel',
+      link: function(scope, element, attrs, ngModel) {
+        ngModel.$parsers.push(function(value) { 
+          ngModel.$setValidity('match', value == scope.$eval(attrs.matchValidator));
+          return value;
+        });
       }
     }
-  );
+  }
+);
 
 })();
 
