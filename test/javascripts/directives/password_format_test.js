f73092475fdd4a299cfe40372b77a48b75270532
Patrick O'Brien <obiedman@gmail.com>
2014-07-01 11:34:13 -0400

    Tests for password validation

---
+++ test/javascripts/directives/password_format_test.js
@@ -0,0 +1,37 @@
+//=require test_helper
+
+describe('password format validation', function() {
+  var $scope, form;
+  beforeEach(module('IntervalBraining'));
+  beforeEach(inject(function($compile, $rootScope) {
+    $scope = $rootScope.$new();
+    var element = angular.element('<form name="userForm">' + 
+      '<input ng-model="model.password" name="password" password-format-validator />' + 
+      '</form>');
+    $scope.model = {password : null};
+    $compile(element)($scope);
+    form = $scope.userForm;
+  }));
+
+  it('should fail when password length < 8 characters', function() {
+    form.password.$setViewValue('t1.esT');
+    $scope.$digest();
+    expect($scope.model.password).toBeUndefined();
+    expect(form.password.$valid).toBe(false);
+  });
+
+  it('should fail when missing required characters', function() {
+    form.password.$setViewValue('testTest');
+    $scope.$digest();
+    expect($scope.model.password).toBeUndefined();
+    expect(form.password.$valid).toBe(false);
+  });
+
+
+  it('should pass when requirements are met', function() {
+    form.password.$setViewValue('test1.Test');
+    $scope.$digest();
+    expect($scope.model.password).toBe('test1.Test');
+    expect(form.password.$valid).toBe(true);
+  });
+});
---
+++ test/javascripts/directives/password_match_test.js
@@ -0,0 +1,32 @@
+//=require test_helper
+
+describe('password match validation', function() {
+  var $scope, form;
+  beforeEach(module('IntervalBraining'));
+  beforeEach(inject(function($compile, $rootScope) {
+    $scope = $rootScope.$new();
+    var element = angular.element('<form name="userForm">' + 
+      '<input ng-model="model.password" name="password" />' + 
+      '<input ng-model="model.passwordConfirmation" name="password_confirmation" match-validator="model.password" />' +
+      '</form>');
+    $scope.model = {password : null,
+                    passwordConfirmation : null};
+    $compile(element)($scope);
+    form = $scope.userForm;
+  }));
+
+    it('should return true when passwords match', function() {
+      form.password.$setViewValue('test1.Test');
+      form.password_confirmation.$setViewValue('test1.Test');
+      $scope.$digest();
+      expect(form.password_confirmation.$valid).toBe(true);
+    });
+
+    it('should return false and undefined when passwords do not match', function() {
+      form.password.$setViewValue('test1.Test');
+      form.password_confirmation.$setViewValue('test1.test');
+      $scope.$digest();
+      expect($scope.model.passwordConfirmation).toBeUndefined();
+      expect(form.password_confirmation.$valid).toBe(false);
+    });
+});
